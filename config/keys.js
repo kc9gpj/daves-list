d26a9491e2bf8be847b5f99591347e5f1e25c73b
David Hoffmann <dave@dhoff.net>
2020-12-20 10:03:41 -0600

    update keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,13 @@
 // add this file to .gitignore
+const aws = require('aws-sdk');
+
+let s3 = new aws.S3({
+  clientID: process.env.clientID,
+  clientSecret: process.env.clientSecret,
+  dbURI: process.env.dbURI,
+  cookieKey: process.env.cookieKey
+});
+
 module.exports = {
     google: {
         clientID: "646189365515-8kbfmofo5nihrs3hrcoukasu47rp6rfo.apps.googleusercontent.com",
