898101a6c53d894ea56a87bfc4af80e560452bc8
David Hoffmann <dave@dhoff.net>
2022-12-11 13:41:52 -0600

    revert

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,13 @@
 module.exports = {
     google: {
-        clientID: "43984348946-v5bkev5uglvifjoscg0pvj4d4qi2n8k1.apps.googleusercontent.com",
-        clientSecret: "lHAUTAHDRHs81m9CL-8axa5m"
+        clientID: process.env.clientID,
+        clientSecret: process.env.clientSecret
     },
     mongo: {
-        dbURI: "mongodb+srv://daveslist:daveslist123@cluster0.3yl9h.mongodb.net/?retryWrites=true&w=majority"
+        dbURI: process.env.dbURI
     },
     session: {
-        cookieKey: "tryandguessmyidformycookieiditwillbeencyrpted"
+        cookieKey: process.env.cookieKey
 
     }
 };
