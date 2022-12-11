803d62feea4ce3675cb04fc83c815985e605b27d
David Hoffmann <dave@dhoff.net>
2022-12-11 13:13:37 -0600

    add build

---
+++ build/precache-manifest.1b7704e2b2b520cc1f104f35eb06c69e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ece7af8cfaf0a7d7ca3",
+    "url": "/Wustix/ProjectFun2/static/js/main.3bfdba1e.chunk.js"
+  },
+  {
+    "revision": "f8cc0c3d6dcdf717b191",
+    "url": "/Wustix/ProjectFun2/static/js/runtime~main.2d5c1392.js"
+  },
+  {
+    "revision": "149db34d02849ce2d894",
+    "url": "/Wustix/ProjectFun2/static/js/2.23f1365d.chunk.js"
+  },
+  {
+    "revision": "9ac17a3df8ad40a10e5299f375b22403",
+    "url": "/Wustix/ProjectFun2/index.html"
+  }
+];
